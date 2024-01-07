6b52cb88d227ddc09d010b1d810b516bc2922592
Jabrayilzade Ali <jabrayilzadeali@protonmail.com>
2024-01-07 21:54:39 +0400

    Added tailwindcss, removed default styles, improved performance

---
+++ tailwind.config.mjs
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		fontFamily: {
+			sans: [
+				'system-ui',
+				'-apple-system',
+				'BlinkMacSystemFont',
+				'Segoe UI',
+				'Roboto',
+				'Oxygen',
+				'Ubuntu',
+				'Cantarell',
+				'Open Sans',
+				'Helvetica Neue',
+				'sans-serif',
+			]
+		},
+		extend: {},
+	},
+	plugins: [],
+}
