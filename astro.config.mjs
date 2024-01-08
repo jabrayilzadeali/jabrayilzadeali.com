b65a066d70962e2f46bfca0c4e3ec6729b4240f7
Jabrayilzade Ali <jabrayilzadeali@protonmail.com>
2024-01-08 17:02:24 +0400

    now layout looks a lot better

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,19 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
+import { defineConfig } from "astro/config"
+import mdx from "@astrojs/mdx"
+import sitemap from "@astrojs/sitemap"
+import tailwind from "@astrojs/tailwind"
 
-import tailwind from "@astrojs/tailwind";
+import icon from "astro-icon"
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
-  integrations: [mdx(), sitemap(), tailwind({applyBaseStyles: false})]
-});
+    site: "https://example.com",
+    integrations: [
+        mdx(),
+        sitemap(),
+        tailwind({
+            applyBaseStyles: false,
+        }),
+        icon(),
+    ],
+})
