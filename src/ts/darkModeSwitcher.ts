e755bb497688fe64054bc430ce57e5bde83a0175
Jabrayilzade Ali <jabrayilzadeali@protonmail.com>
2024-01-09 13:21:41 +0400

    dark mode switcher is working correctly

---
+++ src/ts/darkModeSwitcher.ts
@@ -0,0 +1,39 @@
+document.addEventListener('astro:page-load', () => {
+
+    // On page load or when changing themes, best to add inline in `head` to avoid FOUC
+    const html = document.querySelector("html")!
+
+    const darkLightModeToggle = document.querySelector("[data-dark-light-mode-toggle]")!
+    const iconDark = document.querySelector("[data-icon=dark]")!
+    const iconLight = document.querySelector("[data-icon=light]")!
+
+    darkLightModeToggle.addEventListener("click", () => {
+        html.classList.toggle("dark")
+        html.classList.toggle("light")
+        if (localStorage.theme === "dark") {
+            localStorage.theme = "light"
+            iconDark.classList.toggle("hidden")
+            iconLight.classList.toggle("hidden")
+        } else {
+            localStorage.theme = "dark"
+            iconDark.classList.toggle("hidden")
+            iconLight.classList.toggle("hidden")
+
+        }
+    })
+
+    console.log(`localstorage.theme ${localStorage.theme}`)
+    if (
+        localStorage.theme === "dark" ||
+        (!("theme" in localStorage) &&
+            window.matchMedia("(prefers-color-scheme: dark)").matches)
+    ) {
+        localStorage.setItem("theme", "dark")
+        iconLight.classList.toggle("hidden")
+    } else {
+        localStorage.setItem("theme", "light")
+        iconDark.classList.toggle("hidden")
+    }
+    
+    html.classList.toggle(localStorage.theme === "dark"? "dark" : "light")
+})
