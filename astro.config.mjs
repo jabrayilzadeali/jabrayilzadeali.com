4e2f6c33f8bda50b6b2730bd50b7d1b7556a815c
Jabrayilzade Ali <jabrayilzadeali@protonmail.com>
2024-06-03 23:55:31 +0400

    c

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,10 +4,14 @@ import sitemap from "@astrojs/sitemap"
 import tailwind from "@astrojs/tailwind"
 
 import icon from "astro-icon"
+import { remarkReadingTime } from './remark-reading-time.mjs';
 
 // https://astro.build/config
 export default defineConfig({
     site: "https://www.jabrayilzadeali.com/",
+    markdown: {
+        remarkPlugins: [remarkReadingTime],
+    },
     integrations: [
         mdx(),
         sitemap(),
--- src/content/config.ts
+++ src/content/config.ts
@@ -10,7 +10,8 @@ const blog = defineCollection({
 		pubDate: z.coerce.date(),
 		updatedDate: z.coerce.date().optional(),
 		heroImage: z.string().optional(),
-		tags: z.array(z.string())
+		tags: z.array(z.string()),
+		// minutesToRead: z.string()
 	}),
 });
 
